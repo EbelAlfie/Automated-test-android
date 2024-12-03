b421757d9fc66fc4aadf99e986b85fc8e53568ff
kelsha <intern.kelsha@gli.id>
2024-12-03 17:12:58 +0700

    add add to cart test

--- src/test/java/testmodule/AndroidTest.java
+++ src/test/java/testmodule/AndroidTest.java
@@ -6,12 +6,15 @@ import base.ConfigConsumer;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.time.Duration;
+import java.util.List;
 
 public class AndroidTest extends ConfigConsumer implements BaseTestModule {
     AndroidDriver driver = null;
@@ -32,8 +35,8 @@ public class AndroidTest extends ConfigConsumer implements BaseTestModule {
 
         options.setCapability("platformName", "Android");
         options.setCapability("appium:automationName","UiAutomator2");
-        options.setCapability("deviceName", "e1sxxx");
-        options.setCapability("udid", "RRCX30BP22A");
+        options.setCapability("deviceName", "jackpotltexx");
+        options.setCapability("udid", "5200802feeb735cf");
         options.setCapability("appPackage", "com.alfamart.alfagift.debug");
         options.setCapability("appActivity", "com.alfamart.alfagift.screen.splash.SplashActivity");
 
@@ -48,9 +51,8 @@ public class AndroidTest extends ConfigConsumer implements BaseTestModule {
             login(driver);
 
 //            //Test Case 2: Add to Cart
-//            login(driver);
-//            addToCart(driver);
-//
+            addToCart(driver);
+
 //            //Test Case 3: Check Out
 //            login(driver);
 //            addToCart(driver);
@@ -66,31 +68,33 @@ public class AndroidTest extends ConfigConsumer implements BaseTestModule {
     }
 
     private void checkout(AndroidDriver driver) {
-        driver.findElement(AppiumBy.id("com.alfamart.alfagift.debug:id/iv_basket")).click();
+        driver.findElement(AppiumBy.id("iv_basket")).click();
     }
 
     private void addToCart(AndroidDriver driver) {
-        driver.findElement(AppiumBy.androidUIAutomator(
-                "new UiScrollable(new UiSelector().scrollable(true).instance(0))" +
-                        ".scrollIntoView(new UiSelector().resourceId(\"com.alfamart.alfagift.debug:id/tv_see_all\"))"));
-
-        driver.findElement(AppiumBy.id("com.alfamart.alfagift.debug:id/tv_see_all")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("clickable_promo")));
+        driver.findElement(AppiumBy.id("clickable_promo")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("buttonCancel")));
+        driver.findElement(AppiumBy.id("buttonCancel")).click();
+        driver.findElement(AppiumBy.id("btn_add_to_cart")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("btn_add_to_cart")));
+        driver.findElement(AppiumBy.id("btn_increase_qty")).click();
     }
 
     private void login(AndroidDriver driver) {
-        driver.findElement(AppiumBy.id("com.android.permissioncontroller:id/permission_allow_button")).click();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("com.alfamart.alfagift.debug:id/btn_login")));
-        driver.findElement(AppiumBy.id("com.alfamart.alfagift.debug:id/btn_login")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("com.alfamart.alfagift.debug:id/et_phone")));
-        driver.findElement(AppiumBy.id("com.alfamart.alfagift.debug:id/et_phone")).sendKeys("081586196863");
-        driver.findElement(AppiumBy.id("com.alfamart.alfagift.debug:id/et_password")).sendKeys("Kelsha123!");
-        driver.findElement(AppiumBy.id("com.alfamart.alfagift.debug:id/btn_next")).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("com.alfamart.alfagift.debug:id/btn_later")));
-        driver.findElement(AppiumBy.id("com.alfamart.alfagift.debug:id/btn_later")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("btn_login")));
+        driver.findElement(AppiumBy.id("btn_login")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("com.android.packageinstaller:id/permission_allow_button")));
+        driver.findElement(AppiumBy.id("com.android.packageinstaller:id/permission_allow_button")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("et_phone")));
+        driver.findElement(AppiumBy.id("et_phone")).sendKeys("081586196863");
+        driver.findElement(AppiumBy.id("et_password")).sendKeys("Kelsha123!");
+        driver.findElement(AppiumBy.id("btn_next")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(AppiumBy.id("btn_later")));
+        driver.findElement(AppiumBy.id("btn_later")).click();
     }
-
-//            driver.findElement(AppiumBy.id("com.android.permissioncontroller:id/permission_allow_foreground_only_button")).click();
 
     @Override
     public void afterTest() {
